ecae16729b7a85862834cc24870f136c5df93129
NextGenTechBar <ngtb@gonzaga.edu>
2021-06-25 13:31:08 -0700

    Add files via upload

--- esp32_ota/esp32_ota.ino
+++ esp32_ota/esp32_ota.ino
@@ -4,15 +4,16 @@
 #include <WiFiClientSecure.h>
 #include "cert.h"
 
-const char * ssid = "home_wifi";
-const char * password = "helloworld";
+
+const char * ssid = "Gonzaga Guest";
+const char * password = "";
 
 
 String FirmwareVer = {
   "2.3"
 };
-#define URL_fw_Version "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/bin_version.txt"
-#define URL_fw_Bin "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/fw.bin"
+#define URL_fw_Version "https://raw.githubusercontent.com/NextGenTechBar/ESP8266_ESP32_SelfUpdate/master/esp32_ota/bin_version.txt"
+#define URL_fw_Bin "https://raw.githubusercontent.com/NextGenTechBar/ESP8266_ESP32_SelfUpdate/master/esp32_ota/fw.bin"
 
 //#define URL_fw_Version "http://cade-make.000webhostapp.com/version.txt"
 //#define URL_fw_Bin "http://cade-make.000webhostapp.com/firmware.bin"
